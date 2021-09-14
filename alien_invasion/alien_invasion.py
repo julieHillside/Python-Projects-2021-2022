b0e712f2409562ae2975017bd04e66e6da6ddae7
Julie Wright <julie@hillsidesc.org>
2021-09-14 13:29:24 -0700

    added ship to the screen

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -6,6 +6,8 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
+
 class AlienInvasion:
     '''Overall class to manage game assets and behavior'''
     
@@ -17,6 +19,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         
+        self.ship = Ship(self)
+        
         
     def run_game(self):
         while True:
@@ -27,6 +31,7 @@ class AlienInvasion:
                     
                     
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
---
+++ alien_invasion/ship.py
@@ -0,0 +1,22 @@
+# from Python Crash Course
+
+# Image by <a href="https://pixabay.com/users/dawnydawny-2157612/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2442125">dawnydawny</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2442125">Pixabay</a>
+
+import pygame
+
+class Ship:
+    def __init__(self, ai_game):
+        '''initialize the ship and set its starting position'''
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        
+        # Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        
+        #start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+    
+    def blitme(self):
+        '''draw the ship at its current location'''
+        self.screen.blit(self.image, self.rect)
