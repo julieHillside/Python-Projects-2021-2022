a737169f11b5a739aa11d7c1668de88f5541dbca
Julie Wright <julie@hillsidesc.org>
2021-09-15 13:12:35 -0700

    added left motion

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -36,9 +36,13 @@ class AlienInvasion:
                 if event.key == pygame.K_RIGHT:
                     #move ship to the right.
                     self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
         
     def run_game(self):
         while True:
