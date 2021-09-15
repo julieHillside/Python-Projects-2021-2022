64e714c9433ef0edb219b9b0526a2ecb6d1ed9a2
Julie Wright <julie@hillsidesc.org>
2021-09-15 12:47:37 -0700

    refactored code

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -17,23 +17,26 @@ class AlienInvasion:
         
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
-        pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_caption("Alien Invasion Game")
         
         self.ship = Ship(self)
+    
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
         
+    def _check_events(self):
+        #watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
         
     def run_game(self):
         while True:
-            #watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                    
-                    
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
