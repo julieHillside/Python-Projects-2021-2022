647c28cb4d02e4623f7f22d90c610401241dfced
Julie Wright <julie@hillsidesc.org>
2021-09-16 13:59:20 -0700

    Added ability to fire bullets

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -8,6 +8,8 @@ from settings import Settings
 
 from ship import Ship
 
+from bullet import Bullet
+
 class AlienInvasion:
     '''Overall class to manage game assets and behavior'''
     
@@ -20,10 +22,13 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion Game")
         
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
     
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Make the most recently drawn screen visible.
         pygame.display.flip()
         
@@ -34,6 +39,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
     
@@ -43,6 +50,11 @@ class AlienInvasion:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+            
+    def _fire_bullet(self):
+        '''create a new bullet and add it to the bullets group'''
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
         
     def _check_events(self):
         #watch for keyboard and mouse events.
@@ -59,6 +71,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
 if __name__ == '__main__':
