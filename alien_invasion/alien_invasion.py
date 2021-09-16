3a96ca0463fdc6aafdf47d0b6e0601d789de5c03
Julie Wright <julie@hillsidesc.org>
2021-09-16 14:37:37 -0700

    Only allow 3 bullets at a time

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -53,8 +53,9 @@ class AlienInvasion:
             
     def _fire_bullet(self):
         '''create a new bullet and add it to the bullets group'''
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
         
     def _check_events(self):
         #watch for keyboard and mouse events.
