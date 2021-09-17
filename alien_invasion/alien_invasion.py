08944da7e116e24bb7769195b6bac5984e7dfa99
Julie Wright <julie@hillsidesc.org>
2021-09-17 14:03:44 -0700

    created the first alien!

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -10,6 +10,8 @@ from ship import Ship
 
 from bullet import Bullet
 
+from alien import Alien
+
 class AlienInvasion:
     '''Overall class to manage game assets and behavior'''
     
@@ -23,7 +25,16 @@ class AlienInvasion:
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
         
+        self._create_fleet()
+    
+    def _create_fleet(self):
+        '''Create a fleet of aliens'''
+        #Make an alien
+        alien = Alien(self)
+        self.aliens.add(alien)
+    
     def _update_bullets(self):
         '''update position of bullets and get rid of old bullets.'''
         #update bullet positions
@@ -42,6 +53,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         # Make the most recently drawn screen visible.
         pygame.display.flip()
         
