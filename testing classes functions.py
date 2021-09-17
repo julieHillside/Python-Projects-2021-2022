761497bcf3d38aab7acd54f8b09af716ea3d4693
Julie Wright <julie@hillsidesc.org>
2021-09-17 14:32:53 -0700

    Create testing classes functions.py

---
+++ testing classes functions.py
@@ -0,0 +1,9 @@
+class Testing:
+    def __init__(self):
+        self.hello = "hello"
+        self.say_hi()
+        
+    def say_hi(self):
+        print("Hello there")
+        
+test = Testing()
