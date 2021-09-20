7fef6cd504ad62d503ef1e5a66bfe820d222eba5
Julie Wright <julie@hillsidesc.org>
2021-09-20 14:35:15 -0700

    added alien fleet

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -33,7 +33,17 @@ class AlienInvasion:
         '''Create a fleet of aliens'''
         #Make an alien
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2*alien_width)
+        number_aliens_x = available_space_x // (2*alien_width)
+        
+        #Create the first row of aliens
+        for alien_number in range(number_aliens_x):
+            #create an alien and place it in the row.
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
     
     def _update_bullets(self):
         '''update position of bullets and get rid of old bullets.'''
