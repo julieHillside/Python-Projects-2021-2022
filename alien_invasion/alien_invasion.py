9072fc94631dfe8953cc1f0df26025d83b4aa937
Julie Wright <julie@hillsidesc.org>
2021-09-20 15:09:07 -0700

    create rows of aliens

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -30,25 +30,33 @@ class AlienInvasion:
         self._create_fleet()
     
 
-    def _create_alien(self, alien_number):
+    def _create_alien(self, alien_number, row_number):
         '''create an alien and place it in a'''
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width +2*alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
         
        
     
     def _create_fleet(self):
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2*alien_width)
         number_aliens_x = available_space_x // (2*alien_width)
-        #Create the first row of aliens
-        for alien_number in range(number_aliens_x):
-            #create an alien and place it in the row.
-            self._create_alien(alien_number)
+        
+        #Determine the number of rows of aliens that fit on the screen.
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height - (3*alien_height)- ship_height)
+        number_rows = available_space_y // (2*alien_height)
+        
+        #Create full fleet of aliens
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                #create an alien and place it in the row.
+                self._create_alien(alien_number, row_number)
     
     def _update_bullets(self):
         '''update position of bullets and get rid of old bullets.'''
