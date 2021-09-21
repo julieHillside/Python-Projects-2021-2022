cd4ff7bac67af1e541c1e1c2a281dc9db48c0129
Julie Wright <julie@hillsidesc.org>
2021-09-21 13:50:24 -0700

    fleet changes direction and drops

--- alien_invasion/alien.py
+++ alien_invasion/alien.py
@@ -21,6 +21,13 @@ class Alien(Sprite):
         self.x = float(self.rect.x)
         
     def update(self):
-        '''Move the alien to the right'''
-        self.x += self.settings.alien_speed
+        '''Move the alien to the right or left'''
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
+    
+    def check_edges(self):
+        '''return True if alien is on the edge of the screen'''
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+        
--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -116,8 +116,23 @@ class AlienInvasion:
                self._check_keyup_events(event)
     
     def _update_aliens(self):
-        '''update the positions of all the aliens in the fleet'''
+        '''check if the fleet is at and edge then
+            update the positions of all the aliens in the fleet'''
+        self._check_fleet_edges()
         self.aliens.update()
+        
+    def _check_fleet_edges(self):
+        '''respond appropriately if an alien reaches the edge'''
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        '''drop the entire fleet and change the fleet's direction'''
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
         
     def run_game(self):
         while True:
